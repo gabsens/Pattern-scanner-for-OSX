8fed8fc74e339a75937569217bfbf3d2df25be24
gabsens <gabriel.romon@ensae.fr>
2016-07-05 15:44:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 //Not finished yet
+#include <cstdio>
+#include <cstdlib>
 
 ////
 // define global patterns
 ////
+
+////
+// copy client.dylib to our program memory
+////
+
+uint64_t Client;
+uint64_t Size;
+
+////
+// search for patterns and get offsets
+////
+
+uint64_t
